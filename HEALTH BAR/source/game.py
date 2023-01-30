fc66e5f72155867e31cbf9294a4a9e272c3e3efe
PDT-Bin <phuduthang300502@gmail.com>
2023-01-30 17:15:23 +0700

    UPDATE HEALTH BAR

--- HEALTH BAR/source/game.py
+++ HEALTH BAR/source/game.py
@@ -8,12 +8,13 @@ class StaticHealthBar:
 
     def __init__(self):
         # CORE.
-        BAR_WIDTH, BAR_HEIGHT = 400, 25
+        self.image = pg.image.load('image/HEALTH_BAR.png').convert_alpha()
+        self.rect = self.image.get_rect(topleft=(25, 25))
+        # INFORMATION.
+        BAR_WIDTH, BAR_HEIGHT = 156, 7
         self.MAX_HEALTH, self.cur_health = 1000, 500
+        self.health_rect = pg.Rect(25 + 32, 25 + 27, 0, BAR_HEIGHT)
         self.RATIO = BAR_WIDTH / self.MAX_HEALTH
-        # RECT.
-        self.rect = pg.Rect(25, 25, 0, BAR_HEIGHT)
-        self.frame_rect = pg.Rect(25, 25, BAR_WIDTH, BAR_HEIGHT)
 
     def update(self, quantity: int):
         self.cur_health += quantity
@@ -26,61 +27,62 @@ class StaticHealthBar:
         return int(value * self.RATIO)
 
     def draw(self):
-        # UPDATE INFORMATION.
-        self.rect.width = self.ratio(self.cur_health)
+        # UPDATE RECT.
+        self.health_rect.width = self.ratio(self.cur_health)
         # DRAW.
-        pg.draw.rect(screen, '#FF0000', self.rect)
-        pg.draw.rect(screen, '#FFFFFF', self.frame_rect, 3)
+        screen.blit(self.image, self.rect)
+        pg.draw.rect(screen, 'RED', self.health_rect)
 
 
 class AnimatingHealthBar:
 
     def __init__(self):
         # CORE.
-        BAR_WIDTH, BAR_HEIGHT = 400, 25
+        self.image = pg.image.load('image/HEALTH_BAR.png').convert_alpha()
+        self.rect = self.image.get_rect(topleft=(25, 75))
+        # INFORMATION.
+        BAR_WIDTH, BAR_HEIGHT = 156, 7
         self.MAX_HEALTH, self.cur_health = 1000, 0
+        self.health_rect = pg.Rect(25 + 32, 75 + 27, 0, BAR_HEIGHT)
         self.RATIO = BAR_WIDTH / self.MAX_HEALTH
-        # RECT
-        self.rect = pg.Rect(25, 75, 0, BAR_HEIGHT)
-        self.frame_rect = pg.Rect(25, 75, BAR_WIDTH, BAR_HEIGHT)
         # ANIMATION.
-        self.aim_health, self.SPEED = 500, 5
-        self.gap_rect = pg.Rect(0, 75, 0, BAR_HEIGHT)
+        self.tar_health, self.SPEED = 500, 5
+        self.gap_rect = pg.Rect(0, 75 + 27, 0, BAR_HEIGHT)
 
     def update(self, quantity: int):
-        self.aim_health += quantity
-        if self.aim_health < 0:
-            self.aim_health = 0
-        if self.aim_health > self.MAX_HEALTH:
-            self.aim_health = self.MAX_HEALTH
+        self.tar_health += quantity
+        if self.tar_health < 0:
+            self.tar_health = 0
+        elif self.tar_health > self.MAX_HEALTH:
+            self.tar_health = self.MAX_HEALTH
 
     def ratio(self, value: int):
         return int(value * self.RATIO)
 
     def draw(self):
         # GAP INFORMATION.
-        gap_color = '#FF0000'
-        # GET HEALTH | DAMAGE.
-        if self.cur_health < self.aim_health:
+        gap_color = 'RED'
+        # UPDATE HEALTH.
+        if self.cur_health < self.tar_health:
             self.cur_health += self.SPEED
-            gap_color = '#00FF00'
-        elif self.cur_health > self.aim_health:
+            gap_color = 'GREEN'
+        elif self.cur_health > self.tar_health:
             self.cur_health -= self.SPEED
-            gap_color = '#FFFF00'
-        # UPDATE INFORMATION.
-        self.rect.width = self.ratio(self.cur_health)
-        gap_width = self.ratio(self.aim_health - self.cur_health)
-        self.gap_rect.x = self.rect.right + (0 if gap_width > 0 else gap_width)
+            gap_color = 'YELLOW'
+        # UPDATE RECT.
+        self.health_rect.width = self.ratio(self.cur_health)
+        gap_width = self.ratio(self.tar_health - self.cur_health)
+        self.gap_rect.x = self.health_rect.right + gap_width * (gap_width < 0)
         self.gap_rect.width = abs(gap_width)
         # DRAW.
-        pg.draw.rect(screen, '#FF0000', self.rect)
+        screen.blit(self.image, self.rect)
+        pg.draw.rect(screen, 'RED', self.health_rect)
         pg.draw.rect(screen, gap_color, self.gap_rect)
-        pg.draw.rect(screen, '#FFFFFF', self.frame_rect, 3)
 
 
 # DISPLAY.
 pg.init()
-screen = pg.display.set_mode((450, 125))
+screen = pg.display.set_mode((250, 175))
 pg.display.set_caption('Health Bar Simulation')
 pg.display.set_icon(pg.image.load('image/HEART.png').convert_alpha())
 # SYSTEM.
